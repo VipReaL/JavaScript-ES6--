f222a849668c425d6e8999563ea3c2350804a5ef
Sergey Igoshin <VipReaLWeb@gmail.com>
2024-07-29 18:36:38 +0500

    Компонент 'Счётчик товаров'

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,17 +1,50 @@
 class Products {
 
+    constructor() {
+        this.classNameActive = 'products-element__btn_active'
+        this.labelAdd = 'Добавить в корзину';
+        this.labelRemove = 'Удалить из корзины';
+    }
+
+    handleSetLocationStorage(element, id) {
+        const { pushProduct, products } = localStorageUtil.putProducts(id);
+
+        if (pushProduct === true) {
+            element.classList.add(this.classNameActive);
+            element.innerHTML = this.labelRemove;
+        } else {
+            element.classList.remove(this.classNameActive);
+            element.innerHTML = this.labelAdd;
+        }
+
+        headerPage.render(products.length);
+    }
+
     render() {
+        const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
 
         CATALOG.forEach(({ id, name, price, img}) => {
+            let activeClass = '';
+            let activeText = '';
+
+            if (productsStore.indexOf(id) === -1) {
+                activeText = this.labelAdd;
+            } else {
+                activeClass = ' '+this.classNameActive;
+                activeText = this.labelRemove;
+            }
+
             htmlCatalog += `
                 <li class="products-element">
                     <span class="products-element__name">${name}</span>
                     <img class="products-element__img" src="${img}" />
                     <span class="products-element__price">
-                        💥 ${price.toLocaleString('ru-RU')} руб
+                        🎸 ${price.toLocaleString('ru-RU')} руб
                     </span>
-                    <button class="products-element__btn">Добавить в корзину</button>
+                    <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}')">
+                        ${activeText}
+                    </button>
                 </li>
             `;
         });
